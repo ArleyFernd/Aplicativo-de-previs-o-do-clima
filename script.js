a2a32c939cd361a9517cc1cf7c8726793fa272ae
paranoid.dev <arleydias20@gmail.com>
2022-07-30 13:01:53 -0500

    last update 30/07/2022

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let clima = {
-    "apiKey": "ecccdb6fcf23495cf8f1fdd1a731bded"
-    
+const api = {
+    key: "ecccdb6fcf23495cf8f1fdd1a731bded",
+    base: "https://api.openweathermap.org/data/3.0/",
+    lang: "pt_br",
+    unit: "metric",
 }
+
+const clima = document.queryselector('.clima')
+const cidade = document.querySelector('.cidade')
+const temperatura = document.querySelector('.temperatura')
+const icon = document.querySelector('.icon')
+const descrição = document.querySelector('.descrição')
+const umidade = document.querySelector('.umidade')
+const vento = document.querySelector('.vento')
+const data_and_hour = document.querySelector('.data_and_hour')
+const barra = document.querySelector('.barra_de_pesquisa')
+const botão = document.querySelector('.btn')
